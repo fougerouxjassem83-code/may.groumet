24266a970a376a28f711da5a6abf32133ae78eba
jassem fougeroux <fougerouxjassem83@gmail.com>
2026-01-30 07:45:58 +0100

    ajout des fichier

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
-//ici on vas créer notre application expressjs
 const express = require('express');
 
 
 
 
-//ici j'importe le framwork Expressjs
 const app = express();
 
+// Je précise que les vues sont dans le dossier views
+app.set('views','./views');
 
 
-//on va installer des MIDDELWARRE c'est à dire en définis les routes localhost:2007
-//j'utilise un Middleware1 pour
-app.use((req,res,next) => {
-    console.log("salut c'est encore moi fiji!");
-    
-    next();
-}); 
+app.set('view engine', 'ejs')
 
 
 
 
+// j'ai dit appJS pour qu'il va sur le dossier public.
 
-
-
-//**ici je vais créer des meddleware pour des route en html
-// ca va nous permettre de  */
+app.use(express.static('public'));
 
 
 
@@ -34,43 +26,11 @@ app.use((req,res,next) => {
 
 
 
-
-
-
-
-
-
-
-
-    //**ici on créer des routes */
-app.get('/',(req,res) => {
-    res.write("<h1>bienvenue chez maygouret</h1>");
-    res.end();
+/** */
+app.get('/api/acceuil', (req, res) => {
+    console.log("Je passe dans /api/acceuil");
+    res.render('acceuil');
 });
-
-
-app.get('/api/acceuil',(req,res,) => {
-    console.log("je vais dans l'acceuil");
-    res.write("<p>je suis dans l'acceuil</p>")
-    res.end();
-});
-
-/*
-app.get('/api/plats',(req,res,) => {
-    console.log("je vais dans les plats");
-    res.write("<p>je suis dans les plats</p>")
-    res.end();
-});
-
-app.get('/api/contact',(req,res,) => {
-    console.log("je vais dans le contact");
-    res.write("<p>je suis dans le contact</p>")
-    res.end();
-});
-
-*/
-
-
 
 
 
@@ -78,3 +38,4 @@ app.get('/api/contact',(req,res,) => {
 
 
 module.exports = app;
+
