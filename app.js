2ec9fbcd5088431a2a7b516bd2610d99cc83a006
jassem fougeroux <fougerouxjassem83@gmail.com>
2026-02-16 14:40:59 +0100

    jai rajouter des api route récup les information depuis le front end

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ const mysql2 = require("mysql2");
 const myConnection = require('express-myconnection');
 const connection = require('express-myconnection');
 
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**ce code me permet de récuperer les information saisi  */
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+
+
 //**ici je configure les élements attendus pour se connecter a ma base de données */
 
 const optionConnectionBasedeDonnees =  {
@@ -29,17 +39,6 @@ const optionConnectionBasedeDonnees =  {
 
 //ici on vas utiliser un middleware pour se connecter  à la bdd
 app.use (myConnection(mysql2, optionConnectionBasedeDonnees, "pool"));
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -78,14 +77,6 @@ app.get('/api/acceuil', (req, res) => {
 
 
 
-
-
-
-
-
-
-
-
 ////////////////////////////////////////
 //**je me connecte a a la bdd grace a la méthode get connect// */
 app.get('/api/equipe', (req, res) => {
@@ -113,22 +104,50 @@ app.get('/api/equipe', (req, res) => {
 
 
 
-/////////////////////////////////////////////////////////////////////////²
+/////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+app.post('/api/fournisseur', (req, res) => {
+    console.log("je passe dans la route api rest /api/fournisseur",req.body);
+    console.log("nom fourrnisseur :",req.body.Nom)
+    console.log("prenom fournisseur :",req.body.emal)
+    
+    req.getConnection((erreur, connection) => {
+        if (erreur) {
+            console.log(erreur);
+        } else {
+            connection.query("SELECT * FROM fournisseur", [], (erreur, resultatfournisseur) => {
+                if (erreur){
+                    console.log("Erreur dans la requete", erreur);
+                } else {
+                    console.log("fournisseur : ", resultatfournisseur);
+                    res.render("fournisseur", { resultatfournisseur });
+                }
+            });
+        }
+    });
+});
+
+
 
 
 /*ICI j'ajoute un fournisseur a ma table fournisseur
 "POST" signifie nouveau*/
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 app.post('/api/fournisseur', (req,res) => {
-  res.send("POST reçu");
+  res.send("POST reçu",req.body);
 });
 
 app.get('/api/fournisseur', (req,res) => {
-  res.render('fournisseur');
+  res.render("fournisseur");
 });
+
+//ici j'essaye d'afficherles information
+
 
 
 
